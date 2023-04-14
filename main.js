4f8ae4ef90dd1675589e20e38a975a0ebf0699fe
HanzHaxors <hanzhaxors@gmail.com>
2023-04-14 19:59:07 +0700

    feat: Added capability to remove music video from homepage

---
+++ main.js
@@ -0,0 +1,24 @@
+// For homepage
+const targetNode = document.body;
+const config = { childList: true, subtree: true };
+const callback = (mutationList, observer) => {
+        for (const mutation of mutationList) {
+                process(mutation.addedNodes);
+        }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
+
+function process(nodes) {
+        for (const node of nodes) {
+                node.parentNode?.querySelectorAll('.badge-style-type-verified-artist').forEach(x => {
+                        let parent = x.parentNode;
+                        while (parent && parent.id != "content") {
+                                parent = parent.parentNode;
+                        }
+
+                        parent.remove();
+                });
+        }
+}
